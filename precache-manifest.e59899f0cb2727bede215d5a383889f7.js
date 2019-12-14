8cb6546132969465101d17cf835e4866fbfc1f22
Lennard de Graaf <lennarddegraaf@MacBook-Air-de-Lennard.local>
2019-12-14 16:39:06 +0100

    Updates

---
+++ precache-manifest.e59899f0cb2727bede215d5a383889f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a76b12785d2bcc063af028020c1f2af",
+    "url": "/quantsavenue/index.html"
+  },
+  {
+    "revision": "20a4f847e3dadfea8159",
+    "url": "/quantsavenue/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b547a30c2588d800d837",
+    "url": "/quantsavenue/static/js/2.b1212bfe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quantsavenue/static/js/2.b1212bfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20a4f847e3dadfea8159",
+    "url": "/quantsavenue/static/js/main.0fc766c9.chunk.js"
+  },
+  {
+    "revision": "1769da9a9577d37e21ef",
+    "url": "/quantsavenue/static/js/runtime-main.bba243bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quantsavenue/static/media/logo.5d5d9eef.svg"
+  }
+]);
